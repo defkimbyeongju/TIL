addcdb6c8d082a0817f3fee084592311d4d39d35
qudwn2941@naver.com <qudwn2941@naver.com>
2023-11-07 16:01:37 +0900

    1107

---
+++ Vue/04-single-file-component/vue-project/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>App.Vue</h1>
+  <MyComponent />
+</template>
+
+<script setup>
+// import MyComponent from './components/MyComponent.vue'
+// import MyComponent from 'src/components/MyComponent.vue'
+import MyComponent from '@/components/MyComponent.vue'
+
+</script>
+
+<style scoped>
+
+</style>
