705c67ece19dd46ce5f344e26034cab32ff9e76b
qudwn2941@naver.com <qudwn2941@naver.com>
2023-11-24 16:48:32 +0900

    1124

---
+++ Vue/day6_class/vue-project/src/utils/dongHyeon.js
@@ -0,0 +1,13 @@
+
+const sayT1 = () => {
+  console.log('T1이 최고야!')
+}
+
+const sayDongHyeon = () => {
+  console.log('징동 DOWN!')
+}
+
+// - 전역 객체로, js 파일이 모듈 역할을 할 수 있도록 
+// console.log(module)
+// module.exports = {sayT1, sayDongHyeon}
+export {sayT1, sayDongHyeon}
---
+++ Vue/day6_class/vue-project/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>test</h1>
+  </div>
+</template>
+
+<script setup>
+
+import {sayT1, sayDongHyeon} from '../utils/dongHyeon.js'
+// const {sayT1} = require('../utils/dongHyeon.js')
+
+
+sayT1()
+sayDongHyeon()
+
+</script>
+
+<style scoped>
+
+</style>
